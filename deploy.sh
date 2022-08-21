9a01d0ee9a7bdee3e5c7d6264beb65a3c840a799
devopsreyansh <mydevops0289@gmail.com>
2022-08-21 05:33:42 +0000

    updating to test ssh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+Hi Am updating this file to test the ssh keys
 Hi am updating this file to test the git fetch command
 hi this is the scripting file
 Hi am updating this file to test the pull command 
