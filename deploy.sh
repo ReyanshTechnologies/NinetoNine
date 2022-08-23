39bfcb65453ee7aa734933cb602300b693f388ac
devopsreyansh <110544023+devopsreyansh@users.noreply.github.com>
2022-08-23 06:14:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ Hi Am updating this file to test the ssh keys
 Hi am updating this file to test the git fetch command
 hi this is the scripting file
 Hi am updating this file to test the pull command 
+updating this file to practice  the fetch command
